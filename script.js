d12ca756dc90774a6ca0205509e3b636e565ae27
joyumoekpo <joyumoekpo1@gmail.com>
2023-08-09 10:32:53 -0500

    Changed colors and sound names

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const sounds = ['clap', 'cry', 'gasp', 'laugh', 'wow'];
+const sounds = ["clap", "cry", "gasp", "laugh", "wow"];
 
-sounds.forEach(sound => {
-	const btn = document.createElement('button');
-	btn.classList.add('btn');
+sounds.forEach((sound) => {
+	const btn = document.createElement("button");
+	btn.classList.add("btn");
 
 	btn.innerText = sound;
 
-	document.getElementById('buttons').appendChild(btn);
+	document.getElementById("buttons").appendChild(btn);
 });
